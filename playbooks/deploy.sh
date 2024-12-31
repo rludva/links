38e4e40ad4c4ae6c9bc32810931476efed03dc45
Radomir Ludva <noreplay@radmi.cz>
2024-12-31 03:00:48 +0100

    Correct the playbooks to be used via inventory file where are the core variables for deployment..

---
+++ playbooks/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Inventory file..
+INVENTORY_FILE="inventory.yaml"
+
+# Run the playbooks..
+ansible-playbook -i "$INVENTORY_FILE" "./build.yaml"
+ansible-playbook -i "$INVENTORY_FILE" "./01 - build_resources.yaml"
+ansible-playbook -i "$INVENTORY_FILE" "./02 - get_certificates.yaml"
+ansible-playbook -i "$INVENTORY_FILE" "deploy.yaml"
